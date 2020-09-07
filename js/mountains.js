d5b20945c8091a3fc76e1b6e3cdfc13fe4259553
lawrence ardosa <lawrenceardosa@gmail.com>
2020-09-07 15:35:38 +0800

    commit

--- js/mountains.js
+++ js/mountains.js
@@ -2325,20 +2325,3 @@ $(".accordion").click(function (e) {
     });
   }
 });
-//////////////////////button-click///////
-const buttons = document.querySelectorAll("");
-buttons.forEach((btn) => {
-  btn.addEventListener("click", function (e) {
-    let x = e.clientX - e.target.offsetLeft;
-    let y = e.clientY - e.target.offsetTop;
-
-    let ripples = document.createElement("span");
-    ripples.style.left = x + "px";
-    ripples.style.top = y + "px";
-    this.appendChild(ripples);
-
-    setTimeout(() => {
-      ripples.remove();
-    }, 1000);
-  });
-});
